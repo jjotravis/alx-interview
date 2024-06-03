73e5cd33d803e3469a6edd76716093b9f674d14e
Travis <ondegotravis@gmail.com>
2024-06-03 22:45:43 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,15 @@
 Pascal triangle
 """
 
+
 def pascal_triangle(n):
     """Function to give pascal's triangle with n rows"""
     triangle = []
     for i in range(n):
         row = [1] * (i + 1)
-    
-        for j in range (1, i):
+
+        for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
+
         triangle.append(row)
-    return (triangle)
+    return triangle
