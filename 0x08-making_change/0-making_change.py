3f18735583894f12bd0499c8f708f211d5e324b5
Travis <ondegotravis@gmail.com>
2024-07-25 22:55:07 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+from typing import List
+
+#!/usr/bin/python3
+"""
+Change for the note
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+
+    min_coins = [0] + [float("inf")] * total
+    for coin in coins:
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+    return min_coins[-1] if min_coins[-1] != float("inf") else -1
