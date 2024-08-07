fea7368119fe9262d7777fdf08f9f79aff81f1bf
Travis <ondegotravis@gmail.com>
2024-08-08 00:03:38 +0300

    First line

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
 def isWinner(x, nums):
     """
     Determines the winner of a game played between Maria and Ben over x rounds.
-    In each round, they take turns picking prime numbers from a set of consecutive integers starting from 1 up to n,
-    removing the chosen prime and its multiples. The player who cannot make a move loses.
+    In each round, they take turns picking prime numbers from
+    a set of consecutive integers starting from 1 up to n,
+    removing the chosen prime and its multiples.
+    The player who cannot make a move loses.
     The function returns the name of the player who wins the most rounds.
     """
+
     def findPrimes(num):
         if num < 2:
             return 0
@@ -18,18 +27,18 @@ def isWinner(x, nums):
             p += 1
         return sum(primes)
 
-    players = {'Maria': 0, 'Ben': 0}
-    
+    players = {"Maria": 0, "Ben": 0}
+
     for n in nums:
         primes_count = findPrimes(n)
         if primes_count % 2 == 0:
-            players['Ben'] += 1
+            players["Ben"] += 1
         else:
-            players['Maria'] += 1
+            players["Maria"] += 1
 
-    if players['Maria'] > players['Ben']:
-        return 'Maria'
-    elif players['Maria'] < players['Ben']:
-        return 'Ben'
+    if players["Maria"] > players["Ben"]:
+        return "Maria"
+    elif players["Maria"] < players["Ben"]:
+        return "Ben"
     else:
         return None
